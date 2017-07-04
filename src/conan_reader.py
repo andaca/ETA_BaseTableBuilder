a82a5e3fde10d02a8693f9f9fa5f6864c5bbdc3e
conanmartin <conanmartin@gmail.com>
2017-07-04 14:12:47 +0100

    Working on filtering based on distance to 2012 stops

---
+++ src/conan_reader.py
@@ -0,0 +1,49 @@
+import json
+from pprint import pprint
+import csv
+from geopy.distance import distance as dist
+import pandas as pd
+
+# def reencode(file):
+# 		yield file.decode('latin-1').encode('utf-8')
+#
+# stop_table = csv.reader(reencode(open('../../../stopinfo.csv')), delimiter=",", quotechar='"')
+
+b = open('../../../data/siri.20121106.csv')
+s = open('../../../stopinfo.csv')
+bus_table = csv.reader(b)
+stop_table = csv.reader(s)
+
+# df = pd.read_csv(bus_table)
+# df = df[pd.notnull(df[3])]
+
+
+with open('../../../routes.txt') as data_file:
+	route_data = json.load(data_file)
+
+# BusRecord = namedtuple('BusRecord',
+#                        ['timestamp', 'lineId', 'direction', 'journeyPatternId',
+#                         'timeFrame', 'vehicleJourneyId', 'operator', 'conjestion',
+#                         'lon', 'lat', 'delay', 'blockId', 'vehicleId', 'stopId',
+#                         'atStop'])
+
+
+# StopRecord = namedtuple('StopRecord', ['number', 'name', 'locality', 'locality_num', 'code', 'stop_no', 'n_class', 'n_id', 'easting', 'northing', 'lat', 'lon'])
+b_time, b_line_id, b_direction, b_jour_patt_id, b_timeframe, b_veh_jour_id, b_oper, b_conj, b_lon, b_lat, b_delay, b_block_id, b_veh_id, b_stop_id, b_at_stop = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
+
+s_num, s_name, s_locality, s_locality_num, s_stop_no, s_easting, s_northing, s_lat, s_lon = 0, 1, 2, 3, 4, 5, 6, 7, 8
+
+
+for row in bus_table:
+	if row[b_jour_patt_id] != 'null' and row[b_jour_patt_id] != '':
+		print("J_ID is", row[b_jour_patt_id])
+		scheduled_stops = route_data[row[b_jour_patt_id]]['stops']
+		for stops_row in stop_table:
+			if int(stops_row[s_stop_no]) in scheduled_stops:
+				print('Found', stops_row[s_stop_no], 'at lat:', stops_row[s_lat], ', lon:', stops_row[s_lon])
+
+
+# pprint(route_data)
+
+s.close()
+b.close()
--- src/transform_data.py
+++ src/transform_data.py
@@ -3,6 +3,8 @@ import csv
 import gzip
 from os import mkdir
 from os.path import isdir
+import json
+from pprint import pprint
 
 from geopy.distance import distance as dist
 
@@ -10,11 +12,12 @@ from filereader import read_csv
 
 
 class Cfg:
-    bus_data_file = '../data/siri.20130101.csv.gz'
-    stops_data_file = '../data/2017_stops.csv'
-    bus_stop_radius = 20  # meters. Arbitraliy chosen.
-    out_dir = '../data/out'
-    out_file = 'out.csv'
+	bus_data_file = '../../../data/siri.20130101.csv'
+	stops_data_file = '../../../stopinfo.csv'
+	bus_stop_radius = 100  # meters. Arbitraliy chosen.
+	out_dir = '../data/out'
+	out_file = 'out.csv'
+	pick_route = '00010001'
 
 
 # Using the namedtuple makes the code more readable, but we aren't using it a lot
@@ -25,40 +28,45 @@ BusRecord = namedtuple('BusRecord',
                         'lon', 'lat', 'delay', 'blockId', 'vehicleId', 'stopId',
                         'atStop'])
 
-StopRecord = namedtuple('StopRecord', ['id', 'name', 'lat', 'lon'])
+StopRecord = namedtuple('StopRecord', ['number', 'name', 'locality', 'locality_num', 'code', 'stop_no', 'n_class', 'n_id', 'easting', 'northing', 'lat', 'lon'])
 
 
 def main():
-    bus_data = read_csv(Cfg.bus_data_file, BusRecord)
-    stop_data = list(read_csv(Cfg.stops_data_file, StopRecord))
+	bus_data = read_csv(Cfg.bus_data_file, BusRecord)
+	stop_data = list(read_csv(Cfg.stops_data_file, StopRecord))
+	with open('../../../routes.txt') as data_file:
+		data = json.load(data_file)
 
-    routes = defaultdict(list)
-    count = 0
 
-    # very inefficient, to run this for the entire dataset will take > a day
-    for bus in bus_data:
-        for stop in stop_data:
-            if dist((bus.lat, bus.lon), (stop.lat, stop.lon)).meters < Cfg.bus_stop_radius:
-                print('\n{} : {}'.format(stop.name, stop.id))
+	routes = defaultdict(list)
+	count = 0
 
-                new_row = (bus.vehicleId, bus.lineId, bus.timestamp,
-                           stop.id, stop.name, stop.lat, stop.lon)
+	# for x in stop_data:
+	# 	print(x.stop_no)
 
-                routes[bus.vehicleJourneyId].append(new_row)
+	# very inefficient, to run this for the entire dataset will take > a day
+	for bus in bus_data:
+		for stop in stop_data:
+			if dist((bus.lat, bus.lon), (stop.lat, stop.lon)).meters < Cfg.bus_stop_radius:
+				print('\n{} : {}'.format(stop.name, stop.id))
 
-        count += 1
-        print('\r{} rows processed'.format(count), end='')
+				new_row = (bus.vehicleId, bus.lineId, bus.timestamp,
+                   	       stop.id, stop.name, stop.lat, stop.lon)
 
-    if not isdir(Cfg.out_dir):
-        mkdir(Cfg.out_dir)
+				routes[bus.vehicleJourneyId].append(new_row)
+		count += 1
+		print('\r{} rows processed'.format(count), end='')
 
-    # Maybe incorporate this code into the block above?
-    for k, v in routes.items():
-        filename = k + '.csv'
-        with open(filename, 'wt') as f:
-            writer = csv.writer(f)
-            for row in v:
-                writer.writerow(row)
+	# if not isdir(Cfg.out_dir):
+	# 	mkdir(Cfg.out_dir)
+
+	# Maybe incorporate this code into the block above?
+	# for k, v in routes.items():
+	# 	filename = k + '.csv'
+	# 	with open(filename, 'wt') as f:
+	# 		writer = csv.writer(f)
+	# 		for row in v:
+	# 			writer.writerow(row)
 
 
 if __name__ == '__main__':
